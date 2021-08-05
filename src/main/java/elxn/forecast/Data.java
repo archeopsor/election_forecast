d0b872d65e42c7a30ee9722a0e627c75acefd1af
archeopsor <simirsingh@gmail.com>
2021-08-05 11:52:26 -0400

    convert polls to tables

--- src/main/java/elxn/forecast/Data.java
+++ src/main/java/elxn/forecast/Data.java
@@ -1,38 +1,36 @@
 package elxn.forecast;
 
 import elxn.forecast.Poll;
+import tech.tablesaw.api.Table;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 
 public class Data {
-    private static final String presidentPrimaryPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/president_primary_polls.csv";
-    private static final String presidentGeneralPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/president_polls.csv";
-    private static final String presidentApprovalPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/president_approval_polls.csv";
-    private static final String senatePollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/senate_polls.csv";
-    private static final String housePollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/house_polls.csv";
-    private static final String genericBallotPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/generic_ballot_polls.csv";
+    private static String presidentPrimaryPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/president_primary_polls.csv";
+    private static String presidentGeneralPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/president_polls.csv";
+    private static String presidentApprovalPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/president_approval_polls.csv";
+    private static String senatePollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/senate_polls.csv";
+    private static String housePollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/house_polls.csv";
+    private static String genericBallotPollsUrl = "https://projects.fivethirtyeight.com/polls-page/data/generic_ballot_polls.csv";
 
-    private static void getPolls(String FILE_URL) throws IOException {
+    private static Table getPolls(String FILE_URL) throws IOException {
         try {
             URL url = new URL(FILE_URL);
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-
-            String inputLine;
-            while ((inputLine = in.readLine()) != null)
-                System.out.println(inputLine);
-            in.close();
+            Table dataframe = Table.read().csv(url);
+            return dataframe;
         } catch (MalformedURLException e) {
             throw new IOException("Unable to download file: " + FILE_URL);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        getPolls(presidentPrimaryPollsUrl);
+        System.out.println(getPolls(presidentPrimaryPollsUrl));
     }
 }
--- src/main/java/elxn/forecast/Poll.java
+++ src/main/java/elxn/forecast/Poll.java
@@ -2,8 +2,11 @@ package elxn.forecast;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.text.DateFormat;
 import java.text.ParseException;
+
+// This is useless for now because of tablesaw 
 
 public class Poll {
 
@@ -46,43 +49,134 @@ public class Poll {
     String candidate_party;
     Float pct;
 
-    public Poll(ArrayList<String> pollData) throws ParseException {
-        question_id = Integer.parseInt(pollData.get(0));
-        poll_id = Integer.parseInt(pollData.get(1));
-        cycle = Integer.parseInt(pollData.get(2));
-        state = pollData.get(3);
-        pollster_id = Integer.parseInt(pollData.get(4));
-        pollster = pollData.get(5);
-        sponsor_id = Integer.parseInt(pollData.get(6));
-        sponsors = pollData.get(7);
-        display_name = pollData.get(8);
-        pollster_rating_id = Integer.parseInt(pollData.get(9));
-        pollster_rating_name = pollData.get(10);
-        fte_grade = pollData.get(11);
-        sample_size = Integer.parseInt(pollData.get(12));
-        population = pollData.get(13);
-        population_full = pollData.get(14);
-        methodology = pollData.get(15);
-        office_type = pollData.get(16);
-        seat_number = Integer.parseInt(pollData.get(17));
-        seat_name = pollData.get(18);
-        start_date = DateFormat.getDateInstance().parse(pollData.get(19));
-        end_date = DateFormat.getDateInstance().parse(pollData.get(20));
-        election_date = DateFormat.getDateInstance().parse(pollData.get(21));
-        sponsor_candidate = pollData.get(22);
-        internal = Boolean.parseBoolean(pollData.get(23));
-        partisan = pollData.get(24);
-        tracking = pollData.get(25);
-        nationwide_batch = Boolean.parseBoolean(pollData.get(26));
-        ranked_choice_reallocated = Boolean.parseBoolean(pollData.get(27));
-        created_at = DateFormat.getDateInstance().parse(pollData.get(28));
-        notes = pollData.get(29);
-        url = pollData.get(30);
-        stage = pollData.get(31);
-        race_id = Integer.parseInt(pollData.get(32));
-        answer = pollData.get(33);
-        candidate_id = Integer.parseInt(pollData.get(34));
-        candidate_name = pollData.get(35);
-        pct = Float.parseFloat(pollData.get(36)) / 100;
+    public Poll(String[] header, String[] pollData) throws ParseException {
+        // Map with array index as key, and the relevant column as value
+        // Since not all csv files from 538 have the same order of columns
+        HashMap<Integer, String> columnMap = new HashMap<Integer, String>();
+        for (int i = 0; i < header.length; i++) {
+            columnMap.put(i, header[i]);
+        }
+        
+        for (int i = 0; i < pollData.length; i++) {
+            // Get rid of empty values in poll data
+            if (pollData[i].isBlank()) {
+                pollData[i] = "0";
+            }
+
+            // Set poll values
+            // I don't know a better way to do this
+            String col = columnMap.get(i);
+            if (col.equals("question_id")) {
+                question_id = Integer.parseInt(pollData[0]);
+            }
+            if (col.equals("poll_id")) {
+                poll_id = Integer.parseInt(pollData[1]);
+            }
+            if (col.equals("cycle")) {
+                cycle = Integer.parseInt(pollData[2]);
+            }
+            if (col.equals("state")) {
+                state = pollData[3];
+            }
+            if (col.equals("pollster_id")) {
+                pollster_id = Integer.parseInt(pollData[4]);
+            }
+            if (col.equals("pollster")) {
+                pollster = pollData[5];
+            }
+            if (col.equals("sponsor_id")) {
+                sponsor_id = Integer.parseInt(pollData[6]);
+            }
+            if (col.equals("sponsors")) {
+                sponsors = pollData[7];
+            }
+            if (col.equals("display_name")) {
+                display_name = pollData[8];
+            }
+            if (col.equals("question_id")) {
+                pollster_rating_id = Integer.parseInt(pollData[9]);
+            }
+            if (col.equals("question_id")) {
+                pollster_rating_name = pollData[10];
+            }
+            if (col.equals("question_id")) {
+                fte_grade = pollData[11];
+            }
+            if (col.equals("question_id")) {
+                sample_size = Integer.parseInt(pollData[12]);
+            }
+            if (col.equals("question_id")) {
+                population = pollData[13];
+            }
+            if (col.equals("question_id")) {
+                population_full = pollData[14];
+            }
+            if (col.equals("question_id")) {
+                methodology = pollData[15];
+            }
+            if (col.equals("question_id")) {
+                office_type = pollData[16];
+            }
+            if (col.equals("question_id")) {
+                seat_number = Integer.parseInt(pollData[17]);
+            }
+            if (col.equals("question_id")) {
+                seat_name = pollData[18];
+            }
+            if (col.equals("question_id")) {
+                start_date = DateFormat.getDateInstance().parse(pollData[19]);
+            }
+            if (col.equals("question_id")) {
+                end_date = DateFormat.getDateInstance().parse(pollData[20]);
+            }
+            if (col.equals("question_id")) {
+                election_date = DateFormat.getDateInstance().parse(pollData[21]);
+            }
+            if (col.equals("question_id")) {
+                sponsor_candidate = pollData[22];
+            }
+            if (col.equals("question_id")) {
+                internal = Boolean.parseBoolean(pollData[23]);
+            }
+            if (col.equals("question_id")) {
+                partisan = pollData[24];
+            }
+            if (col.equals("question_id")) {
+                tracking = pollData[25];
+            }
+            if (col.equals("question_id")) {
+                nationwide_batch = Boolean.parseBoolean(pollData[26]);
+            }
+            if (col.equals("question_id")) {
+                ranked_choice_reallocated = Boolean.parseBoolean(pollData[27]);
+            }
+            if (col.equals("question_id")) {
+                created_at = DateFormat.getDateInstance().parse(pollData[28]);
+            }
+            if (col.equals("question_id")) {
+                notes = pollData[29];
+            }
+            if (col.equals("question_id")) {
+                url = pollData[30];
+            }
+            if (col.equals("question_id")) {
+                stage = pollData[31];
+            }
+            if (col.equals("question_id")) {
+                race_id = Integer.parseInt(pollData[32]);
+            }
+            if (col.equals("question_id")) {
+                answer = pollData[33];
+            }
+            if (col.equals("question_id")) {
+                candidate_id = Integer.parseInt(pollData[34]);
+            }
+            if (col.equals("question_id")) {
+                candidate_name = pollData[35];
+            }
+            if (col.equals("question_id")) {
+                pct = Float.parseFloat(pollData[36]) / 100;
+            }
+        }
     }
 }
